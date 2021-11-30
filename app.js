e4c6beb552ac20585471f8d4953d5ecff6357371
Chris Castle <chris@render.com>
2021-11-30 16:08:57 -0500

    Add confetti 🎉

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require("express");
+
 const app = express();
 const port = process.env.PORT || 3001;
 
-app.get("/", (req, res) => res.send("Hello there!"));
+app.use(express.static('public'))
+
+app.get("/", (req, res) => res.sendFile('index.html', {root: './public'}));
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
